d1b8ecee36023b44ece3f59f2e38bbd76a35ebd6
Omar M. Elgendy <omaratef3221@gmail.com>
2022-12-04 15:58:29 +0400

    adding Tensorflow Notebook in Collab but still working torch errors

--- Text_Toxicity_Torch/get_data.py
+++ Text_Toxicity_Torch/get_data.py
@@ -8,8 +8,8 @@ import torch
 class get_data(Dataset):
     def __init__(self):
         self.data = pd.read_csv("train_data_version3.csv")
-        self.x = self.data["text"]
-        self.y = self.data["y"]
+        self.x = list(self.data["text"])
+        self.y = list(self.data["y"])
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     def get_df(self):
@@ -21,15 +21,17 @@ class get_data(Dataset):
     def get_summary(self):
         return self.data.describe()
     def prepare_data(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.x, self.y, test_size=0.25)
-        X_train = [self.tokenizer(text,padding='max_length', max_length = 512, truncation=True,
-                                return_tensors="pt") for text in list(X_train)]
-        X_test = [self.tokenizer(text, padding='max_length', max_length=512, truncation=True,
-                                  return_tensors="pt") for text in list(X_test)]
+        #X_train, X_test, y_train, y_test = train_test_split(self.x, self.y, test_size=0.25)
+        self.texts = [self.tokenizer(text,
+                                padding='max_length', max_length= 512, truncation=True,
+                                return_tensors="pt") for text in self.x]
+
+        train, test = torch.utils.data.random_split([self.texts, self.y], [0.7, 0.3])
 
 
-        y_train = torch.tensor(list(y_train), dtype=torch.float32)
-        y_test = torch.tensor(list(y_test), dtype=torch.float32)
-        train_dl = DataLoader([X_train, y_train], batch_size=16, shuffle=True)
-        test_dl = DataLoader([X_test, y_test], batch_size=1024, shuffle=False)
+
+        # y_train = torch.tensor(list(y_train), dtype=torch.float32)
+        # y_test = torch.tensor(list(y_test), dtype=torch.float32)
+        train_dl = DataLoader(train, batch_size=16, shuffle=True)
+        test_dl = DataLoader(test, batch_size=16, shuffle=False)
         return train_dl, test_dl
